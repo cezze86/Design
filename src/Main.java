017b89ff9f49045c7343bb05172703af8dc80240
Nora Nassir <noraknassir@gmail.com>
2023-12-13 13:33:16 +0100

    fixed nested while loops

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,28 @@ public class Main {
     public static void main(String[] args) {
 
         Boolean wantsToContinue = true;
-        Boolean wantsToTransfer = false;
+        Boolean wantsToCreateAccount = true;
         BankAccountFactory baf;
         Scanner scan = new Scanner(System.in);
         User user = new User("603580");
 
         String input;
 
-        System.out.println("Skriv 1 för att skapa ett 'Lönekonto' eller 2 för ett 'Sparkonto'");
-        String inputAccountType = scan.nextLine();
+        System.out.println("Välkommen!");
+        while (wantsToCreateAccount) {
+            System.out.println("Skriv 1 för att skapa ett 'Lönekonto' eller 2 för ett 'Sparkonto'");
+            String inputAccountType = scan.nextLine();
 
 
-        if (inputAccountType.equals("1") || inputAccountType.equals("2")) {
-            baf = new BankAccountFactory(Integer.parseInt(inputAccountType), user);
-            user.addBankAccount(baf.getBankAccount());
+            if (inputAccountType.equals("1") || inputAccountType.equals("2")) {
+                baf = new BankAccountFactory(Integer.parseInt(inputAccountType), user);
+                user.addBankAccount(baf.getBankAccount());
 
-            System.out.println(inputAccountType);
-            System.out.println(baf.bankAccount.creationDate);
+                System.out.println("Ditt konto har nu skapats!");
+                System.out.println(baf.bankAccount.creationDate);
 
-            System.out.print("\033[H\033[2J");
-        } else{
+
+            } else {
                 System.out.println("Det blev fel. Försök igen, eller vill du avsluta?(J/N)");
                 input = scan.nextLine();
                 switch (input.toLowerCase()) {
@@ -37,10 +39,10 @@ public class Main {
                 }
             }//End of else clause
 
-        while (wantsToContinue) {
+            while (wantsToContinue) {
 
 
-                System.out.println("1. Insättning\n2. Uttag\n3. Se saldo");
+                System.out.println("1. Insättning\n2. Uttag\n3. Se saldo\n4. Skapa ett sparkonto\n0. Avsluta");
                 input = scan.nextLine();
                 switch (input.toLowerCase()) {
                     case "1":
@@ -49,27 +51,34 @@ public class Main {
                         user.getBankAccount().deposit(Integer.parseInt(input));
                         System.out.println(input + " är nu insatt på ditt konto");
 
-                            break;
+                        break;
 
                     case "2":
                         System.out.println("Hur mycket vill du ta ut?");
                         input = scan.nextLine();
                         user.getBankAccount().withdraw(Integer.parseInt(input));
-                            break;
+                        break;
 
                     case "3":
                         System.out.println(user.getBankAccount().getBalance());
+                        break;
+
+                    case "4":
+                        wantsToCreateAccount = true;
+
+                    case "0":
+                        wantsToContinue = false;
 
                 }
 
-                displayAccounts(user);
+                //displayAccounts(user);
 
 
             }//End of first if case clause
 
         }// First While loop ends here
-    //Main method ends here
-
+        //Main method ends here
+    }
     public static void displayAccounts(User user)
     {
         int i = 1;
